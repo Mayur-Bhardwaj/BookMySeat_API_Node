8be97e25401968c5f3e8f76bab9f841d7a9ead9a
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2026-04-10 17:31:49 +0530

    Create movie model

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const env = require("dotenv");
 env.config();
 const app = express();
+const Movie = require("./models/movie.model");
 
 //  res --> it is sent from the client.
 // res --> when you are going to send to the client when req is completed.
@@ -30,6 +31,18 @@ app.listen(process.env.PORT, async() => {
     try{
   await mongoose.connect(process.env.DB_URL);
   console.log("Successfully connect to mongo");
+
+  // await Movie.create({
+  //   name: "Dhurandar",
+  //   description: "Action Movie",
+  //   casts: ["Ranveer Singh","Akshay Khanna", "Sanjay Dutt", "Saumya Tandon", "Sara Arjun", "Rakesh Bedi"],
+  //   director: "Aditya Dhar",
+  //   trailerUrl: "https://www.youtube.com/watch?v=BKOVzHcjEIo",
+  //   language: "Hindi",
+  //   releaseDate: "05-12-2025",
+  //   releaseStatus: "RELEASED"
+  // });
+
   } catch(err){
   console.log("Not able to connect mongo",err);
   }
